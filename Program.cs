82881c7211eb48ad396a85dfe3f791dfd5058a1b
jorneycr <jorneycr@hotmail.com>
2024-11-27 23:15:07 -0600

    added minor change

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddHttpsRedirection(options =>
 
 // Configura el DbContext para usar SQL Server
 builder.Services.AddDbContext<TravelContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionLocal")));
 
 // Configura Identity con roles
 builder.Services.AddIdentity<Usuario, IdentityRole>()
